18834441d794f38075f30bc62545ffec38a4f144
Cindy Ren <bdcindy78@gmail.com>
2023-06-29 11:00:58 +1000

    card

---
+++ src/travel/Travel.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: false },
+];
+export default function Travel() {
+  return <div>Travel</div>;
+}
